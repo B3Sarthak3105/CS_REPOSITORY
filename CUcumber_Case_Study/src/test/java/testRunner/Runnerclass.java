4f6a8a70435a9ba624e73df020cf3d63e13463b4
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3B-1PHFGP2.dir.svc.accenture.com>
2020-01-29 18:19:50 +0530

    runner class changed

--- CUcumber_Case_Study/src/test/java/testRunner/Runnerclass.java
+++ CUcumber_Case_Study/src/test/java/testRunner/Runnerclass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "featurefile", glue = { "Stepdefinition" }, tags = "@login",plugin = {"preety","jason;target/cucumber.jason"})
+@CucumberOptions(features = "featurefile", glue = { "Stepdefinition" }, tags = "@login", plugin = {"preety", "json:target/cucumber.json"})
 
 public class Runnerclass {
 
